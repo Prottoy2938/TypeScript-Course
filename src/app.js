f56e4201f50e45c0ef4c8898d2c0c70ff5261485
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-03-22 17:06:52 +0600

    Nothing to say

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
-var obj = {
-    name: "Prottay",
-    space: "Yes",
-    hobbies: ["programming", "Reading Space Books"],
-    role: [1, "admin"]
+// const obj: {
+//   name: string;
+//   space: string;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Prottay",
+//   space: "Yes",
+//   hobbies: ["programming", "Reading Space Books"],
+//   role: [1, "admin"]
+// };
+var Role;
+(function (Role) {
+    Role[Role["AUTHOR"] = 54] = "AUTHOR";
+    Role[Role["ADMIN"] = 55] = "ADMIN";
+    Role[Role["READ_ONLY"] = 56] = "READ_ONLY";
+})(Role || (Role = {}));
+var person = {
+    name: "Prottay Rudra",
+    treeLeaves: "Green",
+    data: Role.AUTHOR
 };
-var customizedArray;
-customizedArray = ["hi", "hello", 90];
-for (var _i = 0, _a = obj.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-}
-console.log(customizedArray);
-console.log(obj.name);
-obj.role[1] = "as";
-obj.role.push("hello");
-console.log(obj.role, obj.role.length);
+if (Role.ADMIN === 55)
+    console.log(Role.ADMIN);
--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,48 @@
-const obj: {
-  name: string;
-  space: string;
-  hobbies: string[];
-  role: [number, string];
-} = {
-  name: "Prottay",
-  space: "Yes",
-  hobbies: ["programming", "Reading Space Books"],
-  role: [1, "admin"]
+// const obj: {
+//   name: string;
+//   space: string;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Prottay",
+//   space: "Yes",
+//   hobbies: ["programming", "Reading Space Books"],
+//   role: [1, "admin"]
+// };
+
+// for (const hobby of obj.hobbies) {
+//   console.log(hobby.toUpperCase());
+// }
+
+// obj.role[1] = "as";
+
+// let customizedArray: (string | number)[];
+
+// customizedArray = ["hi", "hello", 90];
+// interface Loose {
+//   [key: string]: string;
+// }
+
+// const data: Loose = { name: "Prottay" };
+
+// data.lastName = "Rudra";
+// data.light = "sun";
+
+// console.log(data);
+
+interface Loose {
+  [key: string]: string | number;
+}
+
+enum Role {
+  AUTHOR = 54,
+  ADMIN,
+  READ_ONLY
+}
+
+const person: Loose = {
+  name: "Prottay Rudra",
+  treeLeaves: "Green",
+  data: Role.AUTHOR
 };
-
-let customizedArray: (string | number)[];
-
-customizedArray = ["hi", "hello", 90];
-
-for (const hobby of obj.hobbies) {
-  console.log(hobby.toUpperCase());
-}
-
-obj.role[1] = "as";
-
-interface looseObject {
-  [key: string]: string;
-}
-
-const data: looseObject = { name: "Prottay" };
-
-data.lastName = "Rudra";
-
-console.log(customizedArray, data);
+if (Role.ADMIN === 55) console.log(Role.ADMIN);
