8b76af17196b06b12454a65dbdedbc73ee94a901
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-03-27 09:33:44 +0600

    Finished video 2 -learned how to auto compile a specific file in typescript using --watch or -w in the command line

--- src/app.js
+++ src/app.js
@@ -1,11 +1,4 @@
 /* eslint-disable no-unused-vars */
-var sum = function (num1, num2) {
-    return num1 + num2;
-};
-function printResult(num) {
-    console.log("Result: ", num);
-}
-printResult(sum(5, 12));
-var combineValues;
-combineValues = sum;
-console.log(combineValues(8, 8));
+//use `tsc src/app.js --watch` to auto compile a file
+var data = "Space X";
+console.log(data);
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,4 @@
 /* eslint-disable no-unused-vars */
-
-function generateErrors(message: string, code: number): never {
-  throw { message, errorCode: code };
-}
-
-generateErrors("please type a valid email", 500);
+//use `tsc src/app.js --watch` to auto compile a file
+const data = "Space X";
+console.log(data);
